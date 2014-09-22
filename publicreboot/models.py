1536897397eb331acc814a0603e904d66c090d13
Michael Williams <draringi@draringi.net>
2014-09-22 01:08:48 -0400

    Adding more views fitting with current website design

--- publicreboot/models.py
+++ publicreboot/models.py
@@ -4,18 +4,20 @@ from django.db import models
 
 class OfficeHours(models.Model)
     DAYS = (
-        ('M', 'Monday'),
-        ('T', 'Tuesday'),
-        ('W', 'Wednesday'),
-        ('R', 'Thursday'),
-        ('F', 'Friday'),
+        ('1', 'Monday'),
+        ('2', 'Tuesday'),
+        ('3', 'Wednesday'),
+        ('4', 'Thursday'),
+        ('5', 'Friday'),
     )
     start_time = models.TimeField()
     end_time = models.TimeField()
-    day = models.CharField(max_length=1, choices=DAYS, unique=True)
+    day = models.SmallPositiveIntegerField(max_length=1, choices=DAYS, unique=True)
     
     def __str__(self):
         return self.day
     
+    class Meta:
+        ordering = ['day']
 
 
--- publicreboot/views.py
+++ publicreboot/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from machinerequests.models import Request
-from django.views.generic import CreateView
+from publicreboot.models import OfficeHours
+from django.views.generic import CreateView, base.TemplateView as TemplateView, DetailView
 
 # Create your views here.
 
 class RequestCreate(CreateView):
     model = Request
     fields = ['given_name', 'family_name', 'email', 'requester_type', 'faculty_and_dept', 'organization', 'preset', 'os', 'machine_use', 'need_display', 'need_mouse', 'need_keyboard', 'need_ethernet', 'extra_information', 'amount']
+
+class RecyclingView(TemplateView):
+    template_name = "publicreboot/recycling.html"
+
+class AboutView(DetailView):
+    template_name = "publicreboot/about.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['office_hours'] = OfficeHours.objects.all()
+        return context
