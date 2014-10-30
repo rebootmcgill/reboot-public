c7aa5ab78ef491d1aba77c840af3b3d5a3b2206f
Michael Williams <draringi@draringi.net>
2014-10-30 11:17:45 -0400

    Fixed Disclaimer link

--- publicreboot/urls.py
+++ publicreboot/urls.py
@@ -5,7 +5,7 @@ from publicreboot.views import RequestCreate
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'reboot.views.home', name='home'),
-    
+
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^request/$', RequestCreate.as_view()),
@@ -16,5 +16,6 @@ urlpatterns += patterns('django.contrib.flatpages.views',
     url(r'^about/$', 'flatpage', {'url': '/about/'}, name='about'),
     url(r'^recycling/$', 'flatpage', {'url': '/recycling/'}, name='recycling'),
     url(r'^request/thanks/$', 'flatpage', {'url': '/request/thanks/'}, name='thanks'),
+    url(r'^request/disclaimer/$', 'flatpage', {'url': '/request/disclaimer/'}, name='disclaimer'),
 )
 
